f3f680cdf86e8df13259d9b57cfc75ccf6d9611a
Onyy03 <134943868+Onyy03@users.noreply.github.com>
2025-11-10 11:54:00 +0100

    bug kamera + menu

--- Wspinaczka3D-OpenGL/Wspinaczka3D-OpenGL/main.cpp
+++ Wspinaczka3D-OpenGL/Wspinaczka3D-OpenGL/main.cpp
@@ -6,58 +6,13 @@
 #include <sstream>
 #include <vector>
 #include <random> // Do generowania liczb losowych
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/constants.hpp>
-
-// Prototypy funkcji
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-
-// Ustawienia okna
-unsigned int SCR_WIDTH = 800;
-unsigned int SCR_HEIGHT = 600;
-
-// Kamera i postaæ
-glm::vec3 eggPosition = glm::vec3(0.0f, 0.0f, 5.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-float cameraDistance = 7.0f; // Lekko oddalamy kamerê, by lepiej widzieæ scenê
-
-// Myszka
-float lastX;
-float lastY;
-bool firstMouse = true;
-float yaw = -90.0f;
-float pitch = 0.0f;
-
-// Czas
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-
-// Fizyka jajka
-float velocityY = 0.0f;
-bool canJump = true;
-const float GRAVITY = -9.8f;
-const float JUMP_FORCE = 5.0f;
-
-// Struktury do zarz¹dzania chmurami
-struct CloudComponent {
-    glm::vec3 offset; // Przesuniêcie wzglêdem œrodka chmury
-    float scale;
-};
-
-struct Cloud {
-    glm::vec3 position;
-    glm::vec3 velocity;
-    std::vector<CloudComponent> components;
-};
-
-std::vector<Cloud> clouds; // Wektor przechowuj¹cy wszystkie aktywne chmury
-float cloudSpawnTimer = 0.0f;
 
 
 // Klasa Shader
@@ -133,6 +88,68 @@ private:
     }
 };
 
+// Prototypy funkcji
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+// Ustawienia okna
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
+
+// NOWE STANY GRY
+enum GameState {
+    GAME_STATE_MENU,
+    GAME_STATE_PLAYING
+};
+GameState currentState = GAME_STATE_MENU;
+
+// NOWE ZMIENNE GLOBALNE DLA UI
+Shader* uiShader = nullptr;
+unsigned int uiVAO, uiVBO;
+unsigned int menuTexture;
+
+// Kamera i postaæ
+glm::vec3 eggPosition = glm::vec3(0.0f, 0.0f, 5.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float cameraDistance = 7.0f; // Lekko oddalamy kamerê, by lepiej widzieæ scenê
+
+// Myszka
+float lastX;
+float lastY;
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+// Czas
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Fizyka jajka
+float velocityY = 0.0f;
+bool canJump = true;
+const float GRAVITY = -9.8f;
+const float JUMP_FORCE = 5.0f;
+
+// Struktury do zarz¹dzania chmurami
+struct CloudComponent {
+    glm::vec3 offset; // Przesuniêcie wzglêdem œrodka chmury
+    float scale;
+};
+
+struct Cloud {
+    glm::vec3 position;
+    glm::vec3 velocity;
+    std::vector<CloudComponent> components;
+};
+
+std::vector<Cloud> clouds; // Wektor przechowuj¹cy wszystkie aktywne chmury
+float cloudSpawnTimer = 0.0f;
+
+
+
+
 // Funkcja do generowania losowej liczby zmiennoprzecinkowej w zakresie
 float randomFloat(float min, float max) {
     return min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
@@ -158,6 +175,41 @@ void spawnCloud() {
 }
 
 
+// NOWA FUNKCJA DO £ADOWANIA TEKSTURY
+unsigned int loadTexture(const char* path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA; // Nasz plik PNG powinien mieæ RGBA (przezroczystoœæ)
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "Blad ladowania tekstury: " << path << std::endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+}
+
 int main()
 {
     // Inicjalizacja GLFW
@@ -182,8 +234,8 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
+    // Domyœlnie startujemy z widocznym kursorem dla menu
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Blad inicjalizacji GLAD!" << std::endl;
         return -1;
@@ -195,6 +247,53 @@ int main()
     srand(static_cast<unsigned int>(time(0)));
 
     Shader ourShader("vertex_shader.glsl", "fragment_shader.glsl");
+
+    // === INICJALIZACJA UI (NOWY KOD) ===
+    uiShader = new Shader("ui_vertex.glsl", "ui_fragment.glsl");
+    // Ustaw macierz projekcji ortograficznej
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
+    uiShader->use();
+    uiShader->setMat4("projection", projection);
+    uiShader->use(); // U¿yj ponownie, aby ustawiæ uniform
+    glUniform1i(glGetUniformLocation(uiShader->ID, "uiTexture"), 0);
+
+
+    // Definiujemy prostok¹t (quad) na dole ekranu
+    // Mo¿esz dostosowaæ te wartoœci, aby zmieniæ pozycjê napisu
+    float width = 500.0f;
+    float height = 500.0f;
+
+    // Teraz oblicz pozycjê na podstawie rozmiaru
+    float x_pos = (SCR_WIDTH / 2.0f) - (width / 2.0f);
+    float y_pos = (SCR_HEIGHT / 2.0f) - (height / 2.0f);
+
+    // x, y, tex_u, tex_v
+    float uiVertices[] = {
+        x_pos,       y_pos + height,   0.0f, 1.0f,
+        x_pos,       y_pos,            0.0f, 0.0f,
+        x_pos + width, y_pos,            1.0f, 0.0f,
+
+        x_pos,       y_pos + height,   0.0f, 1.0f,
+        x_pos + width, y_pos,            1.0f, 0.0f,
+        x_pos + width, y_pos + height,   1.0f, 1.0f
+    };
+
+    glGenVertexArrays(1, &uiVAO);
+    glGenBuffers(1, &uiVBO);
+    glBindVertexArray(uiVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(uiVertices), &uiVertices, GL_STATIC_DRAW);
+    // atrybut pozycji (vec2)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // atrybut tekstury (vec2)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0); // Od³¹cz VAO
+
+    // Wczytaj teksturê menu
+    menuTexture = loadTexture("menu_prompt.png");
+    // === KONIEC INICJALIZACJI UI ===
 
     // === Konfiguracja JAJKA ===
     std::vector<float> eggVertices;
@@ -304,38 +403,66 @@ int main()
 
         processInput(window);
 
-        velocityY += GRAVITY * deltaTime;
-        eggPosition.y += velocityY * deltaTime;
-        if (eggPosition.y < 0.0f) {
-            eggPosition.y = 0.0f;
-            velocityY = 0.0f;
-            canJump = true;
-        }
-
-        cloudSpawnTimer += deltaTime;
-        if (cloudSpawnTimer > 2.0f && clouds.size() < 15) {
-            spawnCloud();
-            cloudSpawnTimer = 0.0f;
-        }
-
-        for (int i = 0; i < clouds.size(); ++i) {
-            clouds[i].position += clouds[i].velocity * deltaTime;
-            if (clouds[i].position.z > 50.0f) {
-                clouds.erase(clouds.begin() + i);
-                i--;
+        // === FIZYKA I LOGIKA GRY (TYLKO W STANIE 'PLAYING') ===
+        if (currentState == GAME_STATE_PLAYING)
+        {
+            velocityY += GRAVITY * deltaTime;
+            eggPosition.y += velocityY * deltaTime;
+            if (eggPosition.y < 0.0f) {
+                eggPosition.y = 0.0f;
+                velocityY = 0.0f;
+                canJump = true;
             }
-        }
 
-        glClearColor(0.53f, 0.81f, 0.92f, 1.0f);
+            cloudSpawnTimer += deltaTime;
+            if (cloudSpawnTimer > 2.0f && clouds.size() < 15) {
+                spawnCloud();
+                cloudSpawnTimer = 0.0f;
+            }
+
+            for (int i = 0; i < clouds.size(); ++i) {
+                clouds[i].position += clouds[i].velocity * deltaTime;
+                if (clouds[i].position.z > 50.0f) {
+                    clouds.erase(clouds.begin() + i);
+                    i--;
+                }
+            }
+        } // === KONIEC BLOKU if (currentState == GAME_STATE_PLAYING) ===
+
+        glClearColor(0.53f, 0.81f, 0.92f, 1.0f); // Ta linijka zostaje bez zmian
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ourShader.use();
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 150.0f);
-        glm::vec3 cameraPos = eggPosition - cameraFront * cameraDistance;
-        glm::mat4 view = glm::lookAt(cameraPos, eggPosition, cameraUp);
+
+        glm::mat4 view;
+        if (currentState == GAME_STATE_PLAYING)
+        {
+            // Kamera pod¹¿aj¹ca za graczem
+            glm::vec3 cameraPos = eggPosition - cameraFront * cameraDistance;
+
+            // Twoja istniej¹ca poprawka kamery
+            const float minCameraHeight = -0.5f;
+            if (cameraPos.y < minCameraHeight)
+            {
+                cameraPos.y = minCameraHeight;
+            }
+            view = glm::lookAt(cameraPos, eggPosition, cameraUp);
+        }
+        else // currentState == GAME_STATE_MENU
+        {
+            // Statyczna kamera w menu, patrz¹ca na scenê
+            glm::vec3 menuCameraPos = glm::vec3(0.0f, 5.0f, 15.0f); // Mo¿esz dostosowaæ
+            // Patrzymy na platformê startow¹ (szeœcian jest na y=-0.2)
+            view = glm::lookAt(menuCameraPos, glm::vec3(0.0f, 2.0f, 0.0f), cameraUp);
+        }
+
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
+
+       
 
         // Rysowanie pod³ogi
         glBindVertexArray(floorVAO);
@@ -373,6 +500,27 @@ int main()
             }
         }
 
+        // === RENDEROWANIE UI 2D (NOWY KOD) ===
+// Musimy w³¹czyæ blending (przezroczystoœæ) i wy³¹czyæ test g³êbi
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glDisable(GL_DEPTH_TEST); // UI jest zawsze na wierzchu
+
+        if (currentState == GAME_STATE_MENU)
+        {
+            uiShader->use();
+            glBindVertexArray(uiVAO);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, menuTexture);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glBindVertexArray(0);
+        }
+
+        // Przywróæ stany OpenGL
+        glDisable(GL_BLEND);
+        glEnable(GL_DEPTH_TEST); // W³¹cz z powrotem dla sceny 3D
+        // === KONIEC RENDEROWANIA UI ===
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -388,6 +536,10 @@ int main()
     glDeleteVertexArrays(1, &sphereVAO);
     glDeleteBuffers(1, &sphereVBO);
     glDeleteBuffers(1, &sphereEBO);
+    glDeleteVertexArrays(1, &uiVAO);
+    glDeleteBuffers(1, &uiVBO);
+    glDeleteTextures(1, &menuTexture);
+    delete uiShader;
 
     glfwTerminate();
     return 0;
@@ -397,29 +549,52 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    const float WALK_SPEED = 2.5f;
-    const float SPRINT_SPEED = 5.0f;
-    float currentSpeed = WALK_SPEED;
-
-    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-        currentSpeed = SPRINT_SPEED;
+    // --- LOGIKA DLA MENU ---
+    if (currentState == GAME_STATE_MENU)
+    {
+        // Jeœli wciœniêto Enter, przejdŸ do stanu gry
+        if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+        {
+            currentState = GAME_STATE_PLAYING;
+            // Ukryj kursor i zablokuj go w oknie
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            // Zresetuj pozycjê myszy, aby unikn¹æ "skoku" kamery
+            firstMouse = true;
+        }
     }
+    // --- LOGIKA DLA GRY ---
+    else if (currentState == GAME_STATE_PLAYING)
+    {
+        // Ca³a dotychczasowa obs³uga ruchu
+        const float WALK_SPEED = 2.5f;
+        const float SPRINT_SPEED = 5.0f;
+        float currentSpeed = WALK_SPEED;
 
-    glm::vec3 forward = glm::normalize(glm::vec3(cameraFront.x, 0.0f, cameraFront.z));
-    glm::vec3 right = glm::normalize(glm::cross(forward, cameraUp));
+        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+            currentSpeed = SPRINT_SPEED;
+        }
 
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) eggPosition += forward * currentSpeed * deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) eggPosition -= forward * currentSpeed * deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) eggPosition -= right * currentSpeed * deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) eggPosition += right * currentSpeed * deltaTime;
+        glm::vec3 forward = glm::normalize(glm::vec3(cameraFront.x, 0.0f, cameraFront.z));
+        glm::vec3 right = glm::normalize(glm::cross(forward, cameraUp));
 
-    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && canJump) {
-        velocityY = JUMP_FORCE;
-        canJump = false;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) eggPosition += forward * currentSpeed * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) eggPosition -= forward * currentSpeed * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) eggPosition -= right * currentSpeed * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) eggPosition += right * currentSpeed * deltaTime;
+
+        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && canJump) {
+            velocityY = JUMP_FORCE;
+            canJump = false;
+        }
     }
 }
 
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    // Ignoruj ruch myszy, jeœli nie jesteœmy w grze
+    if (currentState != GAME_STATE_PLAYING) {
+        return;
+    }
+
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
     if (firstMouse) {
